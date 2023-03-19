c5a0353322cd3caa2c4e1e5f12566ef050987469
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2023-03-19 23:20:42 +0000

    added few pages

--- src/test/java/com/nextbase/pages/StreamPage.java
+++ src/test/java/com/nextbase/pages/StreamPage.java
@@ -3,12 +3,10 @@ package com.nextbase.pages;
 import com.nextbase.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
-    public BasePage(){
-               PageFactory.initElements(Driver.getDriver(), this);
+public class StreamPage {
+    public StreamPage (){
+        PageFactory.initElements(Driver.getDriver(), this);
     }
-
-
 
 
 }
